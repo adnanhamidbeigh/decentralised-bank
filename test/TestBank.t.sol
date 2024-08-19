56e114b3148053c191e5d7d67bc4afaf049a742e
adnanhamidbeigh <adnanhamidbeg@gmail.com>
2024-08-19 10:40:19 +0000

    forge fmt

--- test/TestBank.t.sol
+++ test/TestBank.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import {Test} from "forge-std/Test.sol";
-import {Bank } from "src/Bank.sol";
+import {Bank} from "src/Bank.sol";
 import {DeployBank} from "script/DeployBank.s.sol";
 
 contract TestBank is Test {
@@ -9,33 +10,36 @@ contract TestBank is Test {
     Bank bank;
     address USER = makeAddr("user");
     uint256 public constant STARTING_BALANCE = 100 ether;
-    uint256 public constant AMOUNT = 10 ether;    
+    uint256 public constant AMOUNT = 10 ether;
+
     function setUp() public {
         deployer = new DeployBank();
         bank = deployer.run();
         vm.deal(USER, STARTING_BALANCE);
     }
 
-    function testBankDeposit() external{
+    function testBankDeposit() external {
         vm.startPrank(USER);
         bank.deposit{value: AMOUNT}();
         uint256 balance = bank.getBalance();
         vm.stopPrank();
         assert(balance == AMOUNT);
     }
-    function testBankWithdraw() external{
+
+    function testBankWithdraw() external {
         vm.startPrank(USER);
         bank.deposit{value: AMOUNT}();
-        bank.withdraw(AMOUNT);        
+        bank.withdraw(AMOUNT);
         uint256 balance = bank.getBalance();
         vm.stopPrank();
 
         assert(balance == 0);
         assert(USER.balance == STARTING_BALANCE);
     }
-    function testBankWithdrawWithExcessAmount() external{
-        vm.prank(USER);        
+
+    function testBankWithdrawWithExcessAmount() external {
+        vm.prank(USER);
         vm.expectRevert(Bank.Bank__AmountExceedsBalance.selector);
-        bank.withdraw(AMOUNT);        
+        bank.withdraw(AMOUNT);
     }
 }
